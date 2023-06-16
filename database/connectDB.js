6b85f3b7f2799288da97b2f5404778bee0ea0391
Khomeini N.S.E <81471974+NelsonHennessiAyodeji@users.noreply.github.com>
2023-06-16 20:25:34 +0100

    Project starter

---
+++ database/connectDB.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const db = async (uri) => {
+    try {
+        await mongoose.connect(uri);
+        console.log("Database connection was successful");
+    } catch (error) {
+        throw error
+    }
+}
