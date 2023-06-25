43e3e6350e7bda7b1373eedcf0480de71ceb6436
Khomeini N.S.E <81471974+NelsonHennessiAyodeji@users.noreply.github.com>
2023-06-25 10:13:24 +0100

    Partial update
    Completed the product workflow excluding the delete product and upload image functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,67 @@
+const Product = require('../model/Product');
 const { StatusCodes } = require("http-status-codes")
+const { BadRequestError, NotFoundError } = require('../error');
 
 const getAllProducts = async (req, res) => {
-    res.status(StatusCodes.OK).json("All products");
+    const products = await Product.find({}).populate('user', 'name');
+
+    if(!products){
+        throw new NotFoundError("We could not find any products, maybe try creating some");
+    }
+
+    res.status(StatusCodes.OK).json(products);
 };
 
 const getSingleProducts = async (req, res) => {
-    res.status(StatusCodes.OK).json("All product");
+    const {id: productId} = req.params;
+    const product = await Product.findOne({_id: productId}).populate('user', 'name');
+
+    if(!product){
+        throw new NotFoundError(`There is no product with id: ${productId}`)
+    }
+
+    res.status(StatusCodes.OK).json(product);
 };
 
 const createProduct = async (req, res) => {
-    res.status(StatusCodes.OK).json("All product");
+    const product = await Product.create({
+        ...req.body,
+        user: req.user.userId,
+        featured: false,
+        freeShipping: false,
+        averageRating: 0,
+        numOfReviews: 0
+    });
+
+    if(!product){
+        throw new BadRequestError("Please provide necesary product information");
+    }
+
+    res.status(StatusCodes.CREATED).json(product);
 };
 
 const updateProduct = async (req, res) => {
-    res.status(StatusCodes.OK).json("All product");
+    const {id: productId} = req.params;
+    const product = await Product.findOneAndUpdate({_id: productId}, {...req.body}, {new: true, runValidators: true});
+
+    if(!product){
+        throw new NotFoundError(`Could not find product with id: ${productId}`);
+    }
+
+    res.status(StatusCodes.OK).json(product);
 };
 
 const deleteProduct = async (req, res) => {
-    res.status(StatusCodes.OK).json("All product");
+    const {id: productId} = req.params;
+    const product = await Product.findOne({_id: productId});
+
+    if(!product){
+        throw new NotFoundError(`Could not find product with id: ${productId}`);
+    }
+
+    //await product.remove();
+    
+    res.status(StatusCodes.OK).json({mag:"Deleted a product", product: product});
 };
 
 const uploadImage = async (req, res) => {
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -8,9 +8,10 @@ const {
     deleteProduct,
     uploadImage
 } = require('../controllers/productController');
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 
-router.route('/').get(getAllProducts).post(createProduct);
-router.route('/uploadImage').post(uploadImage);
-router.route('/:id').get(getSingleProducts).put(updateProduct).delete(deleteProduct);
+router.route('/').get(getAllProducts).post(authenticateUser, authorizePermissions('admin'), createProduct);
+router.route('/uploadImage').post(authenticateUser, uploadImage);
+router.route('/:id').get(getSingleProducts).put(authenticateUser, authorizePermissions('admin'), updateProduct).delete(authenticateUser, authorizePermissions('admin'), deleteProduct);
 
 module.exports = router;
